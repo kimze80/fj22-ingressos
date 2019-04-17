138ee8964d016e84563ed9aef6ea6721623b81e3
kimze <kimze80@gmail.com>
2019-04-17 18:54:40 -0300

    resolve conflict

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -1,43 +1,10 @@
 package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import java.math.RoundingMode;
 
 import br.com.caelum.ingresso.model.descontos.Desconto;
-
-public class Ingresso {
-	private Sessao sessao;
-	private BigDecimal preco;
-	
-	
-	/**
-	 * @deprecated hibernate only
-	 */
-	
-	public Ingresso() {
-		
-	}
-	
-	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
-		this.sessao = sessao;
-		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
-		
-	}
-	
-	public BigDecimal getPreco() {
-		return preco.setScale(2, RoundingMode.HALF_UP);
-	}
-	
-	public void setPreco() {
-		this.preco = preco;
-	}
-=======
-=======
->>>>>>> work
-
-import br.com.caelum.ingresso.desconto.Desconto;
 
 public class Ingresso {
 	private Sessao sessao;
@@ -66,8 +33,5 @@ public class Ingresso {
     public void setPreco(BigDecimal preco) {
     	this.preco = preco;
     }
-<<<<<<< HEAD
->>>>>>> Desconto e json
-=======
->>>>>>> work
+
 }
