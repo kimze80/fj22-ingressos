521f5c9f0cf30da1c3504ae66021683cd5d8aa62
kimze <kimze80@gmail.com>
2019-04-16 22:53:35 -0300

    commit ingresso.java

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
+<<<<<<< HEAD
 import java.math.RoundingMode;
 
 import br.com.caelum.ingresso.model.descontos.Desconto;
@@ -31,4 +32,36 @@ public class Ingresso {
 	public void setPreco() {
 		this.preco = preco;
 	}
+=======
+
+import br.com.caelum.ingresso.desconto.Desconto;
+
+public class Ingresso {
+	private Sessao sessao;
+    private BigDecimal preco;
+
+    /**
+    * @deprecated hibernate only
+    */
+    public Ingresso(){
+
+    }
+
+    public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+        this.sessao = sessao;
+        this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+    }
+
+    public Sessao getSessao() {
+        return sessao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+    
+    public void setPreco(BigDecimal preco) {
+    	this.preco = preco;
+    }
+>>>>>>> Desconto e json
 }
