6fb4dbe071d77c195975eff2b09a717707dbc960
kimze <kimze80@gmail.com>
2019-04-17 21:42:05 -0300

    compras de ingresso

--- src/main/java/br/com/caelum/ingresso/desconto/Desconto.java
+++ src/main/java/br/com/caelum/ingresso/desconto/Desconto.java
@@ -3,5 +3,7 @@ package br.com.caelum.ingresso.desconto;
 import java.math.BigDecimal;
 
 public interface Desconto {
+	
 	BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal);
+	String getDescricao();
 }
